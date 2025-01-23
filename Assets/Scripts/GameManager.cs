c0b14376da2ff24b1bc8da6489363fb5e73e6544
choiminsun <backwonand@sookmyung.ac.kr>
2025-01-24 02:15:16 +0900

    Start Canvas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,85 @@
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //UI Animation
+    public Animator information;
+
+    //Scene UI
+    public GameObject Start;
+    public GameObject Game;
+    public GameObject Over;
+
+    //Pilot Information
+    public GameObject inpuWarning;
+
+    public TextMeshProUGUI inputAge;
+
+    string userAge;
+    string userGender = "여성"; //default
+
+    public static GameManager Instance { get; private set; }
+
+    public void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            //DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(this); 
+
+        GameReady();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void skip()
     {
-        
+        information.SetBool("SKIP", true);
+    }
+
+    public void female()
+    {
+        userGender = "여성";
+    }
+
+    public void male()
+    {
+        userGender = "남성";
+    }
+
+    private bool SetInfo()
+    {
+        foreach (char c in inputAge.text) //Does age consist of numbers
+        {
+            if (c >= '0' && c <= '9' || "\0\n \u200B\t".Contains(c))
+                continue;
+
+            inpuWarning.SetActive(true);
+            return false;
+        }
+
+        userAge = inputAge.text;
+        return true;
+    }
+
+    public void GameReady()
+    {
+        Start.SetActive(true);
+        Game.SetActive(false);
+        Over.SetActive(false);
+    }
+
+    public void GameStart()
+    {
+        if (SetInfo())
+        {
+            Start.SetActive(false);
+            Game.SetActive(true);
+            Over.SetActive(false);
+
+            
+        }
     }
 }
