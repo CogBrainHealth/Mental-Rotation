7d00278ebc52777d0bcd24c2d3bfd0de06984bc3
GOHAYOUNG <124705568+GOHAGONG@users.noreply.github.com>
2025-01-23 10:25:00 +0900

    scripts 생성
    Table class 초기화

---
+++ Assets/Scripts/TableGenerator.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Table
+{
+    public string Name { get; set; } // 객체 이름 (e.g., "bear", "cat")
+    public Sprite Image { get; set; } // Unity Sprite (동물 이미지 등)
+    public bool Nullable { get; set; } // 빈 칸 여부
+
+    public Table(string name, Sprite image, bool nullable = false)
+    {
+        Name = name;
+        Image = image;
+        Nullable = nullable;
+    }
+}
+
+public class TableGenerator : MonoBehaviour
+{
+    Table[,] table = new Table[3, 3];
+
+    List<Table> objects = new List<Table> { };
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
