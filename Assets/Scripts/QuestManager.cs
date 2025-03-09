270ff282baf5b1db37caad0b78d257b344126185
고하영 <124705568+GOHAGONG@users.noreply.github.com>
2025-03-09 12:16:22 +0900

    동물 -> 원
    문제 스프라이트 변경

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -22,10 +22,11 @@ public class QuestManager : MonoBehaviour
     public int totalStageNum;
     public TextMeshProUGUI stageNumber; // StageNum UI
 
-    //private List<GameObject> clonedObjects = new List<GameObject>(); //Rotation Table of Ex (Answer List)
+    private List<GameObject> clonedObjects = new List<GameObject>(); //Rotation Table of Ex (Answer List)
 
     private TableController answerTable;
-    private int thisStageNum = 1; //Start at 1 stage
+    //private int thisStageNum = 1; //Start at 1 stage
+    private int thisStageNum = 0;
     private int answer; 
     private float time = 0f;
 
@@ -47,7 +48,7 @@ public class QuestManager : MonoBehaviour
         gm = GameManager.Instance;
         tg = TableGenerator.Instance;
 
-        //PilotDataSetting(); // Allocate quest Data
+        PilotDataSetting(); // Allocate quest Data
     }
 
     public void Update()
@@ -154,102 +155,103 @@ public class QuestManager : MonoBehaviour
         }
 
         gm.StoreScore(new StageScore(thisStageNum, time, correct));
-        Game(); // Next Stage Call
+        //Game(); // Next Stage Call
+        pilotTest();
     }
 
     ////////////////////// Pilot //////////////////////
     //PilotGame Setting
-    //private void PilotDataSetting()
-    //{
-    //    totalStageNum = 10;
+    private void PilotDataSetting()
+    {
+        totalStageNum = 10;
 
-    //    // shuffle stageType
-    //    List<int> stageTypeList = new List<int>(pilotStageType);
+        // shuffle stageType
+        List<int> stageTypeList = new List<int>(pilotStageType);
 
-    //    while (stageTypeList.Count > 0)
-    //    {
-    //        Debug.Log($"stageTypeList.Count: {stageTypeList.Count}");
-    //        int index = Random.Range(0, stageTypeList.Count);
-    //        shuffledStageTypes.Add(stageTypeList[index]);
-    //        stageTypeList.RemoveAt(index);
-    //    }
-    //}
+        while (stageTypeList.Count > 0)
+        {
+            Debug.Log($"stageTypeList.Count: {stageTypeList.Count}");
+            int index = Random.Range(0, stageTypeList.Count);
+            shuffledStageTypes.Add(stageTypeList[index]);
+            stageTypeList.RemoveAt(index);
+        }
+    }
 
-    //// Game Start and Next Game
-    //public void pilotTest()
-    //{
-    //    if (thisStageNum < totalStageNum) //Quest Count Check
-    //    {
-    //        pilotStage(thisStageNum++);
-    //    }
-    //    else
-    //    {
-    //        gm.GameOver();
-    //    }
-    //}
+    // Game Start and Next Game
+    public void pilotTest()
+    {
+        if (thisStageNum < totalStageNum) //Quest Count Check
+        {
+            pilotStage(thisStageNum++);
+        }
+        else
+        {
+            gm.GameOver();
+        }
+    }
 
-    ////this Stage Setting
-    //private void pilotStage(int stageNum)
-    //{
-    //    // Clean existed cloned object
-    //    foreach (GameObject obj in clonedObjects)
-    //    {
-    //        Destroy(obj);
-    //    }
-    //    clonedObjects.Clear();
+    //this Stage Setting
+    private void pilotStage(int stageNum)
+    {
+        // Clean existed cloned object
+        foreach (GameObject obj in clonedObjects)
+        {
+            Destroy(obj);
+        }
+        clonedObjects.Clear();
 
-    //    // stageNumber UI
-    //    stageNumber.text = $"{thisStageNum} / {totalStageNum} ";
+        // stageNumber UI
+        stageNumber.text = $"{thisStageNum} / {totalStageNum} ";
 
-    //    // select stageType
-    //    int stageType = shuffledStageTypes[thisStageNum - 1]; // index니까
-    //    tg.TableGeneratePilot(tableEx, stageType, true); // TableEx
+        // select stageType
+        int stageType = shuffledStageTypes[thisStageNum - 1]; // index니까
+        tg.TableGeneratePilot(tableEx, stageType, true); // TableEx
 
-    //    Debug.Log($"stageType: {stageType}");
+        Debug.Log($"stageType: {stageType}");
 
-    //    // Rotation Table of Ex (Answer List)
-    //    TableController[] answerArray = new TableController[3];
-    //    for (int i = 0; i < 3; i++) // rotation 90, 180, 270
-    //    {
-    //        //generate answer array
-    //        TableController rotatedTable = Instantiate(tableEx, tableEx.transform.parent);
-    //        rotatedTable.RotateTable(i);
-    //        answerArray[i] = rotatedTable;
+        // Rotation Table of Ex (Answer List)
+        TableController[] answerArray = new TableController[3];
+        for (int i = 0; i < 3; i++) // rotation 90, 180, 270
+        {
+            //generate answer array
+            TableController rotatedTable = Instantiate(tableEx, tableEx.transform.parent);
+            rotatedTable.RotateTable(i);
+            answerArray[i] = rotatedTable;
 
-    //        // add in cloned object list
-    //        clonedObjects.Add(rotatedTable.gameObject);
-    //    }
+            // add in cloned object list
+            clonedObjects.Add(rotatedTable.gameObject);
+        }
 
-    //    //select correct answer number
-    //    answer = Random.Range(0, 3);
-    //    Debug.Log($"정답: {answer + 1}");
+        //select correct answer number
+        answer = Random.Range(0, 3);
+        Debug.Log($"정답: {answer + 1}");
 
-    //    //generate choice tables
-    //    for (int i = 0; i < 3; i++)
-    //    {
-    //        if (i == answer) //correct answer
-    //        {
-    //            //정답 테이블 선지로 이동
-    //            int correctAnswerIndex = tg.RotateAngle(stageType);
+        //generate choice tables
+        for (int i = 0; i < 3; i++)
+        {
+            if (i == answer) //correct answer
+            {
+                //정답 테이블 선지로 이동
+                int correctAnswerIndex = tg.RotateAngle(stageType);
 
-    //            TableController answerTable = answerArray[correctAnswerIndex];
-    //            answerTable.transform.position = new Vector3(-1.3f + (1.3f * i), -3.3f, 0);
-    //        }
-    //        else //incorrect answer
-    //        {
-    //            table[i].gameObject.SetActive(true);
-    //            do
-    //            {
-    //                tg.TableGeneratePilot(table[i], stageType, false);
-    //            }
-    //            while ( table[i].CompareTable(tableEx) ||
-    //                    (i > 0 && answer != 0 && table[i].CompareTable(table[0])) ||
-    //                    (i == 2 && answer != 1 && table[i].CompareTable(table[1]))
-    //                   ); // 보기 및 이미 생성된 오답 선지와 같으면 다시 생성
-    //        }
-    //    }
+                TableController answerTable = answerArray[correctAnswerIndex];
+                answerTable.transform.position = new Vector3(-1.3f + (1.3f * i), -3.3f, 0);
+            }
+            else //incorrect answer
+            {
+                table[i].gameObject.SetActive(true);
+                do
+                {
+                    tg.TableGeneratePilot(table[i], stageType, false);
+                }
+                while (table[i].CompareTable(tableEx) ||
+                        (i > 0 && answer != 0 && table[i].CompareTable(table[0])) ||
+                        (i == 2 && answer != 1 && table[i].CompareTable(table[1]))
+                       ); // 보기 및 이미 생성된 오답 선지와 같으면 다시 생성
+            }
+        }
 
-    //    time = 0f;
-    //}
+        time = 0f;
+    }
     ////////////////////// Pilot End //////////////////////
 }
