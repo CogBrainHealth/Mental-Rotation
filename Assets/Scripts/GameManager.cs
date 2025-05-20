cf16e8e149c97223ab3b574b4370dde2dfc0dd2b
고하영 <124705568+GOHAGONG@users.noreply.github.com>
2025-05-20 22:28:02 +0900

    TMP Emoji Setting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -155,7 +155,11 @@ public class GameManager : MonoBehaviour
     private void displayResult()
     {
         ScoreText.text = (Score * 100 / 40).ToString();
-        ScoreBar.fillAmount = Score / 40;
+        Score /= 40;
+        ScoreBar.fillAmount = Score;
+        Score *= 100;
+        
+        Debug.Log("최종 점수: " + Score + "점");
 
         messageTitle.text = "";
         message.text = "";
@@ -165,10 +169,10 @@ public class GameManager : MonoBehaviour
             messageTitle.text += "20대";
 
             message.text += "그림 돌리기 게임을 통해 공간 지각 능력을 검사했어요!\n\n" +
-                            "최고 수준의 공간 지각 능력!\n" +
+                            " <sprite name=\"rocket\">최고 수준의 공간 지각 능력!\n" +
                             "000님은 20대 수준의 공간 지각 능력을 가지고 있어요!\n" +
                             "주어진 공간에서 위치를 빠르게 파악하고, 다양한 형태나 구조를 정확히 이해할 수 있는 능력이 뛰어나네요.\n\n" +
-                            "이 수준을 유지하려면?\n" +
+                            " <sprite name=\"check\">이 수준을 유지하려면?\n" +
                             "- 다양한 공간적 배열을 시각적으로 상상하며 훈련하세요.\n" +
                             "- 3D 퍼즐이나 레고 조립처럼 입체적인 활동을 통해 더욱 향상시킬 수 있어요!";
         }
@@ -178,10 +182,10 @@ public class GameManager : MonoBehaviour
             messageTitle.text += "30대";
 
             message.text += "그림 돌리기 게임을 통해 공간 지각 능력을 검사했어요!\n\n" +
-                             "우수한 공간 지각 능력!\n" +
+                             " <sprite name=\"gift\">우수한 공간 지각 능력!\n" +
                              "000님은 30대 수준의 공간 지각 능력을 가지고 있어요!\n" +
                              "공간을 빠르게 이해하고 구조를 직관적으로 파악하는 능력이 뛰어나며, 여러 방향을 동시에 고려할 수 있어요.\n\n" +
-                             "이 능력을 더 키우려면?\n" +
+                             " <sprite name=\"check\">이 능력을 더 키우려면?\n" +
                              "- 다양한 방향으로 움직이며 공간을 회전시키는 연습을 해보세요.\n" +
                              "- 도형을 그려보며 공간적인 변화를 머릿속으로 상상해보는 것이 효과적입니다.";
         }
@@ -191,10 +195,10 @@ public class GameManager : MonoBehaviour
             messageTitle.text += "40대";
 
             message.text += "그림 돌리기 게임을 통해 공간 지각 능력을 검사했어요!\n\n" +
-                             "균형 잡힌 공간 지각 능력!\n" +
+                             " <sprite name=\"good\">균형 잡힌 공간 지각 능력!\n" +
                              "000님은 40대 수준의 공간 지각 능력을 가지고 있어요!\n" +
                              "전체적인 공간을 잘 인지하고 지각하고 있어요.\n\n" +
-                             "좀 더 향상시키려면?\n" +
+                             " <sprite name=\"check\">좀 더 향상시키려면?\n" +
                              "- 좀 더 복잡한 사물을 활용해보세요.\n" +
                              "- 공간을 인지할 때 이 사물을 돌리면 어떤 모습이 될까 연상해보는 연습이 효과적입니다.";
         }
@@ -204,10 +208,10 @@ public class GameManager : MonoBehaviour
             messageTitle.text += "50대";
 
             message.text += "그림 돌리기 게임을 통해 공간 지각 능력을 검사했어요!\n\n" +
-                             "훈련하면 더 좋아질 수 있어요!\n" +
+                             " <sprite name=\"running\">훈련하면 더 좋아질 수 있어요!\n" +
                              "000님은 50대 수준의 공간 지각 능력을 가지고 있어요!\n" +
                              "공간을 이해하는 데 시간이 조금 더 걸리거나, 경로와 위치를 기억하는 데 어려움이 있을 수 있어요.\n\n" +
-                             "더 나은 능력을 원한다면?\n" +
+                             " <sprite name=\"check\">더 나은 능력을 원한다면?\n" +
                              "- 주변 환경을 이용해보세요!\n" +
                              "  예: \"사과를 거꾸로 뒤집으면  어떤 모양이 될까?.\"\n" +
                              "  이런 방식으로 사물을 여러 방향으로 뒤집어 보면 공간지각능력이 향상됩니다!";
@@ -219,10 +223,10 @@ public class GameManager : MonoBehaviour
             messageTitle.text += "60대";
 
             message.text += "그림 돌리기 게임을 통해 공간 지각 능력을 검사했어요!\n\n" +
-                             "조금 더 자주 훈련하면 좋아질 수 있어요!\n" +
+                             " <sprite name=\"brain\">조금 더 자주 훈련하면 좋아질 수 있어요!\n" +
                              "000님은 60대 수준의 공간 지각 능력을 가지고 있어요!\n" +
                              "공간을 빠르게 이해하는 데 어려움이 있을 수 있으며, 방향을 기억하는 데 시간이 더 걸릴 수 있어요.\n\n" +
-                             "훈련을 위해?\n" +
+                             " <sprite name=\"check\">훈련을 위해?\n" +
                              "- 우리에게 익숙한 사물을 돌려보세요!";
 
         }
@@ -232,10 +236,10 @@ public class GameManager : MonoBehaviour
             messageTitle.text += " 70대";
 
             message.text += "그림 돌리기 게임을 통해 공간 지각 능력을 검사했어요!\n\n" +
-                             "조금 더 연습하면 공간 지각 능력이 향상될 거예요!\n" +
+                             " <sprite name=\"weight\">조금 더 연습하면 공간 지각 능력이 향상될 거예요!\n" +
                              "000님은 70대 수준의 공간 지각 능력을 가지고 있어요!\n" +
                              "공간을 인식하는데 어려움이 있을 수 있지만, 꾸준한 훈련을 통해 점차 개선될 수 있어요.\n\n" +
-                             "훈련을 위해?\n" +
+                             " <sprite name=\"check\">훈련을 위해?\n" +
                              "- 간단한 도형을 돌려보세요!\n" +
                              "- 예를 들어, 간단한 도형을 다향한 방식으로 회전해보세요.";
         }
